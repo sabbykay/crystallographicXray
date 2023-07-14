348254230e73a6c0aac6a0748e30e9754e647028
sabrina <ssullens@bnl.gov>
2023-07-13 22:56:24 -0400

    gave_window_a_title

--- testfiles/hellowWindow.py
+++ testfiles/hellowWindow.py
@@ -1,17 +1,23 @@
 import pygame 
-pygame.init()
 
-width = 800
-height = 600 
-window = pygame.display.set_mode((width, height))
+def main():
+    pygame.init()
 
-running = True
-while running: 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    width = 800
+    height = 600 
+    window = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Empty Display Window") #Set the window title
+
+    running = True
+    while running: 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
     
-    window.fill((0, 0, 0))
-    pygame.display.flip()
+        window.fill((0, 0, 0))
+        pygame.display.flip()
 
-pygame.quit()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
